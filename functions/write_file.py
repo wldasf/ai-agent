8d03e2791b6071e452830187e3d3afe749c330d4
wldasf <waleed.a.assaf@gmail.com>
2026-02-13 01:15:26 +0300

    update

---
+++ functions/write_file.py
@@ -0,0 +1,16 @@
+import os
+
+def write_file(working_directory, file_path, content):
+    try:
+        abs_working_dir = os.path.abspath(working_directory)
+        abs_file_path = os.path.normpath(os.path.join(abs_working_dir, file_path))
+        if os.path.commonpath([abs_working_dir, abs_file_path]) != abs_working_dir:
+            return f'Error: Cannot write to "{file_path}" as it is outside the permitted working directory'
+        if os.path.isdir(abs_file_path):
+            return f'Error: Cannot write to "{file_path}" as it is a directory'
+        os.makedirs(os.path.dirname(abs_file_path), exist_ok=True)
+        with open(abs_file_path, "w") as f:
+            f.write(content)
+        return f'Successfully wrote to "{file_path}" ({len(content)} characters written)'
+    except Exception as e:
+        return f"Error: {e}"
